4d8baef721519aca3ab9c504a30b04dd8700152a
Aniket Verma <bhabhire@gmail.com>
2024-09-02 16:36:55 +0530

    Update code in Assignment 2

--- React/reactassignment2/src/App.js
+++ React/reactassignment2/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { UserProvider } from './UserContext';
+import { ColorProvider } from './ColorContext';
+import LoginComponent from './LoginComponent';
+import WelcomeComponent from './WelcomeComponent';
+import RegistrationComponent from './RegistrationComponent';
+import DisplayComponent from './DisplayComponent';
+import Comp1 from './Comp1';
+import Comp2 from './Comp2';
+import Comp3 from './Comp3';
 
 function App() {
+  const { username, user } = useContext(UserContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorProvider>
+      <UserProvider>
+        <div>
+          {/* Login and Welcome */}
+          {username ? (
+            <div>
+              <WelcomeComponent />
+              <button onClick={() => setUsername('')}>Logout</button>
+            </div>
+          ) : (
+            <LoginComponent />
+          )}
+
+          {/* Registration and Display */}
+          {username && !user ? (
+            <RegistrationComponent />
+          ) : (
+            <DisplayComponent user={user} />
+          )}
+
+          {/* Color Components */}
+          <Comp1 />
+          <Comp2 />
+          <Comp3 />
+        </div>
+      </UserProvider>
+    </ColorProvider>
   );
 }
 
